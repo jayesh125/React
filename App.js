940a4da8a95eebc9c7db4f08feffee48a7d4b10e
Jayesh Shinde <jayesh.v.shinde1251@gmail.com>
2024-06-28 16:35:03 +0530

    About npm

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello world!");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// neasted react element defined
+const heading = React.createElement('h1', {}, 'Hello world!');
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1" }, "Hello world!"),
-    React.createElement("h1", { id: "h1" }, "Hello world!"),
-  ])
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(heading);
-root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# React Projects
+# React
+
+### What Parcel DO?
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- Parcel uses the FILE WATCHING ALGORITHM :-written in C++
+- Caching - for Faster Builds
+- Image Optimization
+- minification of files
+- Bundling of files
+- Compressing of files
+- Consistence Hashing
+- code splitting
+- Differential bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused codes
