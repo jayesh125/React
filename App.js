5e67bb85f9ae684b6d5fcde4694dd6346aea89b1
Jayesh Shinde <jayesh.v.shinde1251@gmail.com>
2024-06-30 10:40:20 +0530

    About functional component

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement('h1', {}, 'Hello world!');
+// React Element
+const heading = React.createElement("h1", { id: "heading" }, "Hello world!");
+console.log(heading);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// JSX - it is not HTML in JS (It is HTML like syntax)
+const jsxheading = <h1 id="heading">Hello world!</h1>;
+console.log(jsxheading);
 
-root.render(heading);
+const title = {
+    text: "Hello world!",
+    id: "heading"
+};
+
+// Functional Component
+const HeadingComponent = () => {
+    return <h1 id="heading1">Hello Jayesh</h1>;
+};
+
+const num = 1000;
+const HeadingComponent2 = () => {
+    // same way to declare without return keyword
+    return (
+        <div className="container">
+            {title.text}
+            <HeadingComponent />
+            <h1 id="heading">Hello world! {num}</h1>
+        </div>
+    );
+};
+
+// This is how you should render functional Component
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent2 />);
